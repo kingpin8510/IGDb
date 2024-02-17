ff8d282ddb2ccfa6e1a0c34063daf68138f4debc
kingpin8510 <rp240479@gmail.com>
2024-02-17 12:18:14 +0530

    Handle game with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,9 +1,13 @@
+import noImage from "../assets/no-image-placeholder.webp";
+
 const getCroppedImage = (url: string) => {
-    if(!url) return ''; //used this so as to avoid the application breaking down without an image
+  // if(!url) return ''; //used this so as to avoid the application breaking down without an image
+  // but now I got the noImage holder.
 
-    const target = 'media/';
-    const index = url.indexOf(target) +target.length;
-    return url.slice(0,index) + 'crop/600/400/' + url.slice(index);
-}
+  if(!url) return noImage;
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+};
 
-export default getCroppedImage
+export default getCroppedImage;
